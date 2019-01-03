762146eb260e40099d1435457e83a27d952190f2
fabio merli <fabio.rilem@gmail.com>
2019-01-03 16:11:50 +0100

    Test Commit from eclipse

--- ForexControllerMicroService/src/main/java/it/test/springboot/microservices/TestExachangeMicroServicesApplication.java
+++ ForexControllerMicroService/src/main/java/it/test/springboot/microservices/TestExachangeMicroServicesApplication.java
@@ -3,6 +3,11 @@ package it.test.springboot.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Fabio Merli
+ *
+ */
 @SpringBootApplication
 public class TestExachangeMicroServicesApplication {
 
